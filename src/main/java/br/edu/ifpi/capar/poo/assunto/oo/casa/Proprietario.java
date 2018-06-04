9661d4ec4ae54aba368c83d500119eabcf727621
Denylson Melo <denylsonmelo@gmail.com>
2018-06-04 20:17:26 -0300

    getters e setters

--- src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Proprietario.java
+++ src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Proprietario.java
@@ -10,15 +10,47 @@ public class Proprietario {
     private Sexo sexo;
     private String endereco;
 
-    public void mudar(String nome){
-        this.nome = nome;
+    public String getSexo() {
+        if(this.sexo == Sexo.FEMININO)
+            return "F";
+        return "M";
     }
-    
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+   
     public void imprimirNome(){
         System.out.println("meu nome é: " + this.nome);
     }
     
-    public Proprietario(String nome) {
+    // Propietario(String)
+     public Proprietario(String nome) {
         this.nome = nome;
+        this.endereco = "parnaiba";
+    }
+    
+    // Propietario(String, String)
+    public Proprietario(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    // Propietario(String, Sexo)
+    public Proprietario(String nome, Sexo sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
     }
 }
--- src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Teste.java
+++ src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Teste.java
@@ -15,21 +15,23 @@ public class Teste {
         
           
         Proprietario proprietario1 = new Proprietario("Vitoria");
-        Proprietario proprietario2 = new Proprietario("Luzia");
-        Proprietario proprietario3 = null;
+        Proprietario proprietario2 = new Proprietario("Luzia", Sexo.FEMININO);
+        Proprietario proprietario3 = new Proprietario("Davi", "na brenha");
         Proprietario proprietario4 = proprietario1;
         
-        proprietario4.mudar("Geyssyele");
+        System.out.println(proprietario2.getNome() + " " + proprietario2.getSexo());
+        System.out.println(proprietario3.getNome() + " " + proprietario3.getSexo() + " " + proprietario3.getEndereco());
         
+        /*
         proprietario1.imprimirNome();
         proprietario2.imprimirNome();
         //proprietario3.imprimirNome();
         proprietario4.imprimirNome();
         
-        
         System.out.println(proprietario1);
         System.out.println(proprietario2);
         System.out.println(proprietario3);
         System.out.println(proprietario4);
-    }
+        */
+}
 }
