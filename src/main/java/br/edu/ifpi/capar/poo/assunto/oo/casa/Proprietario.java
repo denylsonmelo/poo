c2a76a13242151814ae21be117f6af2d1d8853a6
Denylson Melo <denylsonmelo@gmail.com>
2018-06-04 21:40:51 -0300

    tostring, heranca e desafios

--- src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Proprietario.java
+++ src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Proprietario.java
@@ -5,17 +5,44 @@ package br.edu.ifpi.capar.poo.assunto.oo.casa;
  * @author Denylson Melo
  */
 public class Proprietario {
-    
+
     private String nome;
     private Sexo sexo;
     private String endereco;
 
     public String getSexo() {
-        if(this.sexo == Sexo.FEMININO)
-            return "F";
-        return "M";
+        if (null == this.sexo) {
+            return "não especificado";
+        } else {
+            switch (this.sexo) { // this.null.FEMININO
+                case FEMININO:
+                    return "Feminino";
+                case MASCULINO:
+                    return "Masculino";
+                default:
+                    return "não especificado";
+            }
+        }
     }
 
+    //setSexo(String)
+    public void setSexo(String sexo) {
+        // sexo == "feminino"
+        // sexo.equals("feminino")
+        
+        //desafio1: comparar diversos valores, em um conjunto, com o sexo
+        //desafio2: usando lambda 
+        
+        if (sexo.equalsIgnoreCase("feminino")) {
+            this.sexo = Sexo.FEMININO;
+        }
+        else if (sexo.equalsIgnoreCase("masculino")) {
+            this.sexo = Sexo.MASCULINO;
+        }
+
+    }
+
+    //setSexo(Sexo)
     public void setSexo(Sexo sexo) {
         this.sexo = sexo;
     }
@@ -31,17 +58,20 @@ public class Proprietario {
     public String getNome() {
         return nome;
     }
-   
-    public void imprimirNome(){
-        System.out.println("meu nome é: " + this.nome);
+
+    @Override
+    public String toString() {
+        // desafio3: 
+        return "Proprietario[" + this.nome + "," + this.sexo + "," + this.endereco + "]";
     }
-    
+ 
+
     // Propietario(String)
-     public Proprietario(String nome) {
+    public Proprietario(String nome) {
         this.nome = nome;
         this.endereco = "parnaiba";
     }
-    
+
     // Propietario(String, String)
     public Proprietario(String nome, String endereco) {
         this.nome = nome;
--- src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Teste.java
+++ src/main/java/br/edu/ifpi/capar/poo/assunto/oo/casa/Teste.java
@@ -19,19 +19,17 @@ public class Teste {
         Proprietario proprietario3 = new Proprietario("Davi", "na brenha");
         Proprietario proprietario4 = proprietario1;
         
+        
+        /*
+        System.out.println(proprietario1.getNome() + " " + proprietario1.getSexo());
         System.out.println(proprietario2.getNome() + " " + proprietario2.getSexo());
         System.out.println(proprietario3.getNome() + " " + proprietario3.getSexo() + " " + proprietario3.getEndereco());
         
-        /*
-        proprietario1.imprimirNome();
-        proprietario2.imprimirNome();
-        //proprietario3.imprimirNome();
-        proprietario4.imprimirNome();
-        
+        */
+                
         System.out.println(proprietario1);
         System.out.println(proprietario2);
         System.out.println(proprietario3);
         System.out.println(proprietario4);
-        */
 }
 }
